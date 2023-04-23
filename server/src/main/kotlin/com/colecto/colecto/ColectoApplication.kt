81dded16c7d92f88c5e21b95b53e4a09193cb1c3
Henry.K (Seonghoon) Kim <henry.k@hpcnt.com>
2023-04-23 13:57:05 +0900

    boot server

--- server/src/main/kotlin/com/colecto/colecto/ColectoApplication.kt
+++ server/src/main/kotlin/com/colecto/colecto/ColectoApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class ColectoApplication
 
 fun main(args: Array<String>) {
-	runApplication<ColectoApplication>(*args)
+    runApplication<ColectoApplication>(*args)
 }
