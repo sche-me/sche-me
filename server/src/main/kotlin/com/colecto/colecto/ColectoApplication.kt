8b12771b35169507e47a12edba4f494051555542
Henry.K (Seonghoon) Kim <henry.k@hpcnt.com>
2023-04-23 13:41:55 +0900

    init

---
+++ server/src/main/kotlin/com/colecto/colecto/ColectoApplication.kt
@@ -0,0 +1,11 @@
+package com.colecto.colecto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ColectoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ColectoApplication>(*args)
+}
