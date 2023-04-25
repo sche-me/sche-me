1079e5d5739177197c84201ae7af6d0b9eb6804a
eunbbori <eunbbori@gmail.com>
2023-04-25 17:24:12 +0900

    불필요한 문구('컴마') 제거

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Provider } from 'react-redux';
-import { legacy_createStore as createStore } from 'redux';
-import rootReducer from './modules';
+import { Provider } from 'react-redux'
+import { legacy_createStore as createStore } from 'redux'
+import rootReducer from './modules'
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
-    </React.StrictMode>,
+    </React.StrictMode>
   </Provider>
 )
