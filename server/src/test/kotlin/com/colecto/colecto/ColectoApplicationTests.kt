c86fc3979cf1e34d489f196ae70012c7f157a0bd
Henry.K (Seonghoon) Kim <henry.k@hpcnt.com>
2023-04-30 16:33:19 +0900

    fix db connection

--- server/src/test/kotlin/com/colecto/colecto/ColectoApplicationTests.kt
+++ server/src/test/kotlin/com/colecto/colecto/ColectoApplicationTests.kt
@@ -1,9 +1,11 @@
 package com.colecto.colecto
 
 import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ColectoApplicationTests {
 
     @Test
