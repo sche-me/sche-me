1da717350815531dc3a01f186dc36bb4d4e74b36
ksh-code <tjdgnsqn133@gmail.com>
2023-05-07 16:10:33 +0900

    테이블 구조 init

---
+++ server/src/main/resources/db.migration/V202305071600__create_init_tables.sql
@@ -0,0 +1,27 @@
+CREATE TABLE publisher(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    url VARCHAR(1000) NOT NULL,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6)
+) ENGINE = InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+CREATE TABLE rss(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    publisher_id INT UNSIGNED NOT NULL,
+    url VARCHAR(1000) NOT NULL,
+    last_synced_at DATETIME(6) NOT NULL,
+   created_at DATETIME(6) NOT NULL,
+   updated_at DATETIME(6)
+) ENGINE = InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+CREATE TABLE post(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    author VARCHAR(50) NOT NULL,
+    title VARCHAR(1000) NOT NULL,
+    url VARCHAR(1000) NOT NULL,
+    publisher_id INT UNSIGNED NOT NULL,
+    description TEXT,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6)
+) ENGINE = InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
