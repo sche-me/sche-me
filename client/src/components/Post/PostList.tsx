e5bededfe64b8c354f77a2964d59dffd833e2ae3
EUNCHAE KIM <112812483+eunbbori@users.noreply.github.com>
2023-05-14 12:57:27 +0900

    FEATURE - 무한스크롤 구현 수정
    ### Background
    - server와의 통신 후에 무한스크롤이 제대로 구현되지 않음
    
    ### Desc
    - useCallback을 활용해 pageCount가 늘어나는 시점에 loadPostList action 호출

--- client/src/components/Post/PostList.tsx
+++ client/src/components/Post/PostList.tsx
@@ -1,54 +1,43 @@
 import { useDispatch, useSelector } from 'react-redux';
 import allAction from '../../redux/actions';
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { Post } from '../../types/Post'
 import PostItem from './PostItem'
 
 const PostList = () => {
-  const fetchedPosts = useSelector((state:any) => state.postList);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(allAction.loadPostList());
-  }, []);
-
   const [pageCount, setPageCount] = useState(0)
   const [fetching, setFetching] = useState(false)
   const [posts, setPosts] = useState<Post[]>([])
 
   const MAXIMUM_PAGES = 10
 
-  const handleScroll = () => {
-    const scrollHeight = document.documentElement.scrollHeight
-    const scrollTop = document.documentElement.scrollTop
-    const clientHeight = document.documentElement.clientHeight
+  const fetchedPosts = useSelector((state:any) => state.postList);
+  const dispatch = useDispatch();
 
-    if (scrollTop + clientHeight + 100 >= scrollHeight) {
-      fetchMoreData()
-    }
-  }
-
-  window.addEventListener('scroll', handleScroll)
-  useEffect(() => {
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, [])
-
-  const appendData = () => {
-    setFetching(true)
+  const fetchArticle = useCallback(()=> {
+    dispatch(allAction.loadPostList());
     setPosts(fetchedPosts)
     setPageCount(pageCount + 1)
     setFetching(false)
-  }
+  },[pageCount])
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const {scrollHeight, scrollTop, clientHeight } = document.documentElement;
+      if (scrollTop + clientHeight +100 >= scrollHeight) {
+        setFetching(true)
+      }
+    }
+    setFetching(true)
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
 
-  const fetchMoreData = () => {
-    if (pageCount >= MAXIMUM_PAGES) return
-    if (fetching) return
+  useEffect(() => {
+    if (fetching) fetchArticle()
+    else if (pageCount >= MAXIMUM_PAGES) setFetching(false)
+  }, [fetching])
 
-    appendData()
-  }
   return (
     <ul>
       {fetchedPosts.map((post: Post, index: number) =>
